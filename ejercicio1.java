e0120a50a71e3f6ddbf67b514b3b5df3b8eece78
mikefuentesuax <mmurimer@myuax.com>
2024-02-07 18:05:41 +0100

    ej1
    ej1

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,2 +1,6 @@
 public class ejercicio1 {
+    public static void  ejercicio1(String[] arg) {
+        char mayorChar = (char) ((1<<15) - 1);
+        System.out.println("Número mayor entero usando char: ");
+    }
 }
