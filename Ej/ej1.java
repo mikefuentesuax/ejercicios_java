d2622f634ef35ffbd7955ab18482e394803c7c18
mikefuentesuax <146821555+mikefuentesuax@users.noreply.github.com>
2024-02-07 17:19:09 +0000

    Ejercicio 2

---
+++ Ej/ej1.java
@@ -0,0 +1,6 @@
+public class ej1 {
+    public static void main(String[] args) {
+        char mayorChar = (char) ((1<<16) - 1);
+        System.out.println("Número mayor entero usando char: " + (int) mayorChar);
+    }
+}
